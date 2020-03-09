64893475a5112b0d45ee0e9db0af56e3fd38f664
Jeffrey Paulraj <jeffreypaulraj@users.noreply.github.com>
2020-03-09 12:00:30 -0400

    new

--- app/src/main/java/com/example/chatbotapp/MainActivity.java
+++ app/src/main/java/com/example/chatbotapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.chatbotapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView statusText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusText = findViewById(R.id.id_statusText);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    
 }
