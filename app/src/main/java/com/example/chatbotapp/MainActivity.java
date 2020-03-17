e574921a90ce23d9c7ab82e16ea2210e484bf15c
Jeffrey Paulraj <jeffrey.paulraj@gmail.com>
2020-03-16 20:37:39 -0400

    sending messages complete

--- app/src/main/java/com/example/chatbotapp/MainActivity.java
+++ app/src/main/java/com/example/chatbotapp/MainActivity.java
@@ -7,15 +7,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView statusText;
     BroadcastReceiver broadcastReceiver;
     SmsManager smsManager;
+    String senderNumber = "";
+    int currentState = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +38,6 @@ public class MainActivity extends AppCompatActivity {
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         registerReceiver(broadcastReceiver, intentFilter);
-
-        smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("5554", null, "Hi There", null, null);
-
     }
 
     @Override
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     public class TextMonitor extends BroadcastReceiver{
         @Override
         public void onReceive(Context context, Intent intent) {
+            Toast.makeText(MainActivity.this, "Text Received", Toast.LENGTH_SHORT).show();
             Bundle allInfo = intent.getExtras();
             Object[] objectArr = (Object[]) allInfo.get("pdus");
             String format = intent.getStringExtra("format");
@@ -57,8 +60,61 @@ public class MainActivity extends AppCompatActivity {
             }
 
             statusText.setText(smsArr[0].getMessageBody());
+            senderNumber = smsArr[0].getOriginatingAddress();
+
+            Handler handler = new Handler();
+            int timeInterval = (int)Math.random()*5000;
+            handler.postDelayed(sendMessageRunnable(smsArr[0].getMessageBody()), timeInterval);
 
         }
     }
-    
+
+
+    public Runnable sendMessageRunnable(String message){
+        final String receivedMessage = message;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String sendMessage = "";
+                if(currentState == 0){
+                    if(checkForWord(receivedMessage, "Hi")||checkForWord(receivedMessage, "Hello")||checkForWord(receivedMessage, "Hey")){
+                        sendMessage = "Greetings!";
+                    }
+                    else{
+                        sendMessage = "You didn't say hello... How rude!";
+                    }
+                }
+                smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(senderNumber, null, sendMessage, null, null);
+            }
+        };
+
+        return runnable;
+    }
+
+    public boolean checkForWord(String message, String key){
+        ArrayList<String> wordsList = new ArrayList<>();
+        int currentIndex = 0;
+        int numSpaces = 0;
+        for(int i = 0; i < message.length(); i++){
+            if(message.charAt(i) == ' '){
+                wordsList.add(message.substring(currentIndex, i));
+                currentIndex = i;
+                numSpaces++;
+            }
+        }
+        if(numSpaces == 0){
+            wordsList.add(message);
+        }
+
+        for(int i = 0; i < wordsList.size(); i++){
+            if(wordsList.get(i).equalsIgnoreCase(key)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
 }
